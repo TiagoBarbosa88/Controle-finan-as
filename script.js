98f1b7e7729fa5d749f2e6f3029dff7520964043
TiagoBarbosa88 <sombr43@gmail.com>
2022-10-26 07:43:49 -0300

    validation

--- script.js
+++ script.js
@@ -1,28 +1,55 @@
-function validateFields() {
-    const emailValid = isEmailValid();
-    document.getElementById("recover-password-button").disabled = !emailValid;
+const form = {
+  email: () => document.getElementById("email"),
+  emailInvalidError: () => document.getElementById("email-invalid-error"),
+  emailRequiredError: () => document.getElementById("email-required-error"),
+  loginButton: () => document.getElementById("login-button"),
+  password: () => document.getElementById("password"),
+  passwordRequiredError: () =>
+    document.getElementById("password-required-error"),
+  recoverPasswordButton: () =>
+    document.getElementById("recover-password-button"),
+};
 
-    const passwordValid = isPasswordValid();
-    document.getElementById("login-button").disabled = !emailValid || !passwordValid;
+function onChangeEmail() {
+  toggleButtonsDisable();
+  toggleEmailErrors();
+}
 
+function onChangePassword() {
+  toggleButtonsDisable();
+  togglePasswordErrors();
+}
+
+function toggleEmailErrors() {
+  const email = form.email().value;
+  form.emailRequiredError().style.display = email ? "none" : "block";
+
+  form.emailInvalidError().style.display = validateEmail(email)
+    ? "none"
+    : "block";
+}
+
+function togglePasswordErrors() {
+  const password = form.password().value;
+  form.passwordRequiredError().style.display = password ? "none" : "block";
+}
+
+function toggleButtonsDisable() {
+  const emailValid = isEmailValid();
+  form.recoverPasswordButton().disabled = !emailValid;
+
+  const passwordValid = isPasswordValid();
+  form.loginButton().disabled = !emailValid || !passwordValid;
 }
 
 function isEmailValid() {
-    const email = document.getElementById("email").value;
-    if (!email) {
-        return false;
-    }
-    return validateEmail(email);
+  const email = form.email().value;
+  if (!email) {
+    return false;
+  }
+  return validateEmail(email);
 }
 
 function isPasswordValid() {
-    const password = document.getElementById("password").value;
-    if (!password) {
-        return false;
-    }
-    return true;
-}
-
-function validateEmail(email) {
-    return /\S+@\S+\.\S+/.test(email);
+  return form.password().value ? true : false;
 }
