4a6a522fb1329dc3c315d720970f006381008817
TiagoBarbosa88 <sombr43@gmail.com>
2022-10-26 08:30:50 -0300

    Home e Register

--- script.js
+++ script.js
@@ -10,6 +10,14 @@ const form = {
     document.getElementById("recover-password-button"),
 };
 
+function login() {
+  window.location.href = "pages/home/home.html";
+}
+
+function register() {
+  window.location.href = "pages/register/register.html";
+}
+
 function onChangeEmail() {
   toggleButtonsDisable();
   toggleEmailErrors();
