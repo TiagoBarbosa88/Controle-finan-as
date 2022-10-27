804862768919e49964735345345ccf63087148a5
TiagoBarbosa88 <sombr43@gmail.com>
2022-10-27 07:51:38 -0300

    register

---
+++ firebase-init.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyD0uGXTcdeVkxTzmS3A5cUELsDZAboW2ZM",
+    authDomain: "controle-de-gastos-339da.firebaseapp.com",
+    projectId: "controle-de-gastos-339da",
+    storageBucket: "controle-de-gastos-339da.appspot.com",
+    messagingSenderId: "728093315397",
+    appId: "1:728093315397:web:27df1201b0b48ad505ee2c"
+};
+
+firebase.initializeApp(firebaseConfig);
