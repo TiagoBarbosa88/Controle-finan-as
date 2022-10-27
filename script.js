4e8a1e350b8a6d179c130ac6579c8fbc8e51123a
TiagoBarbosa88 <sombr43@gmail.com>
2022-10-26 22:36:31 -0300

    Cadastrando Firebase

--- script.js
+++ script.js
@@ -11,11 +11,48 @@ const form = {
 };
 
 function login() {
-  window.location.href = "pages/home/home.html";
+  showLoading();
+  firebase
+    .auth()
+    .signInWithEmailAndPassword(form.email().value, form.password().value)
+    .then((response) => {
+      hideLoading();
+      window.location.href = "pages/home/home.html";
+    })
+    .catch((error) => {
+      hideLoading();
+      alert(getErrorMessage(error));
+    });
 }
 
 function register() {
+  showLoading();
   window.location.href = "pages/register/register.html";
+}
+
+function recoverPassword() {
+  showLoading();
+  firebase
+    .auth()
+    .sendPasswordResetEmail(form.email().value)
+    .then(() => {
+      hideLoading();
+      alert("Email enviado com sucesso");
+    })
+    .catch((error) => {
+      hideLoading();
+      alert(getErrorMessage(error));
+    });
+}
+
+function getErrorMessage(error) {
+  if (error.code === "auth/user-not-found") {
+    return "Usuário não encontrado";
+  }
+  if (error.code === "auth/wrong-password") {
+    return "Senha inválida";
+  }
+  return error.message;
 }
 
 function onChangeEmail() {
