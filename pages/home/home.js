99a6e30590773783fea10e8fbe0ca34093e31a9d
TiagoBarbosa88 <sombr43@gmail.com>
2022-10-31 22:00:51 -0300

    Atualizando transação

--- pages/home/home.js
+++ pages/home/home.js
@@ -1,82 +1,82 @@
 function logout() {
-  firebase
-    .auth()
-    .signOut()
-    .then(() => {
+  firebase.auth().signOut().then(() => {
       window.location.href = "../../index.html";
-    })
-    .catch(() => {
-      alert("Erro ao fazer logout");
-    });
+  }).catch(() => {
+      alert('Erro ao fazer logout');
+  })
 }
 
-firebase.auth().onAuthStateChanged((user) => {
-  if (user) {
-    findTransactions(user);
+firebase.auth().onAuthStateChanged(user => {
+  if (user){
+      findTransactions(user);
   }
-});
+})
 
-function newTransaction(){
-  window.location.href = '../transaction/transaction.html'
+function newTransaction() {
+  window.location.href = "../transaction/transaction.html";
 }
 
 function findTransactions(user) {
-  showLoading()
-  firebase
-    .firestore()
-    .collection("transactions")
-    .where("user.uid", "==", user.uid)
-    .orderBy("date", "desc")
-    .get()
-    .then((snapshot) => {
-      hideLoading()
-      const transactions = snapshot.docs.map((doc) => doc.data());
-      addTransactionsToScreen(transactions);
-    })
-    .catch(error => {
-      hideLoading()
-      console.log(error);
-      alert('Erro ao recuperar transações')
-    })
+  showLoading();
+  firebase.firestore()
+      .collection('transactions')
+      .where('user.uid', '==', user.uid)
+      .orderBy('date', 'desc')
+      .get()
+      .then(snapshot => {
+          hideLoading();
+          const transactions = snapshot.docs.map(doc => ({
+              ...doc.data(),
+              uid: doc.id
+          }));
+          addTransactionsToScreen(transactions);
+      })
+      .catch(error => {
+          hideLoading();
+          console.log(error);
+          alert('Erro ao recuperar transacoes');
+      })
 }
 
 function addTransactionsToScreen(transactions) {
-  const orderedList = document.getElementById("transactions");
+  const orderedList = document.getElementById('transactions');
 
-  transactions.forEach((transaction) => {
-    const li = document.createElement("li");
-    li.classList.add(transaction.type);
+  transactions.forEach(transaction => {
+      const li = document.createElement('li');
+      li.classList.add(transaction.type);
+      li.addEventListener('click', () => {
+          window.location.href = "../transaction/transaction.html?uid=" + transaction.uid;
+      })
 
-    const date = document.createElement("p");
-    date.innerHTML = formatDate(transaction.date);
-    li.appendChild(date);
+      const date = document.createElement('p');
+      date.innerHTML = formatDate(transaction.date);
+      li.appendChild(date);
 
-    const money = document.createElement("p");
-    money.innerHTML = formatMoney(transaction.money);
-    li.appendChild(money);
+      const money = document.createElement('p');
+      money.innerHTML = formatMoney(transaction.money);
+      li.appendChild(money);
 
-    const type = document.createElement("p");
-    type.innerHTML = transaction.transactionType;
-    li.appendChild(type);
+      const type = document.createElement('p');
+      type.innerHTML = transaction.transactionType;
+      li.appendChild(type);
 
-    if (transaction.description) {
-      const description = document.createElement("p");
-      description.innerHTML = transaction.description;
-      li.appendChild(description);
-    }
+      if (transaction.description) {
+          const description = document.createElement('p');
+          description.innerHTML = transaction.description;
+          li.appendChild(description);
+      }
 
-    orderedList.appendChild(li);
+      orderedList.appendChild(li);
   });
 }
 
 function formatDate(date) {
-  return new Date(date).toLocaleDateString("pt-br");
+  return new Date(date).toLocaleDateString('pt-br');
 }
 
 function formatMoney(money) {
-  return `${money.currency} ${money.value.toFixed(2)}`;
+  return `${money.currency} ${money.value.toFixed(2)}`
 }
-
 /* const fakeTransactions = [
   {
     type: "income",
--- pages/transaction/transaction.js
+++ pages/transaction/transaction.js
@@ -1,45 +1,155 @@
 const form = {
+    currency: () => document.getElementById('currency'),
     date: () => document.getElementById('date'),
-    dateRequiredError: () => document.getElementById('date-required-error'),    
-    value: () => document.getElementById('value'),
-    valueRequiredError: () => document.getElementById('value-required-error'),    
-    valueLessOrEqualToZeroError: () => document.getElementById('value-less-or-equal-to-zero-error'),    
+    description: () => document.getElementById('description'),
+    dateRequiredError: () => document.getElementById('date-required-error'),
+    saveButton: () => document.getElementById('save-button'),
     transactionType: () => document.getElementById('transaction-type'),
     transactionTypeRequiredError: () => document.getElementById('transaction-type-required-error'),
-    saveButton: () => document.getElementById('save-button'),
     typeExpense: () => document.getElementById('expense'),
-    currency: () => document.getElementById('currency'),
-    description: () => document.getElementById('description'),
+    typeIncome: () => document.getElementById('income'),
+    value: () => document.getElementById('value'),
+    valueRequiredError: () => document.getElementById('value-required-error'),
+    valueLessOrEqualToZeroError: () => document.getElementById('value-less-or-equal-to-zero-error')
+}
+if (!isNewTransaction()) {
+    const uid = getTransactionUid();
+    findTransactionByUid(uid);
+}
+
+function getTransactionUid() {
+    const urlParams = new URLSearchParams(window.location.search);
+    return urlParams.get('uid');
+}
+
+function isNewTransaction() {
+    return getTransactionUid() ? false : true;
+}
+
+function findTransactionByUid(uid) {
+    showLoading();
+
+    firebase.firestore()
+        .collection("transactions")
+        .doc(uid)
+        .get()
+        .then(doc => {
+            hideLoading();
+            if (doc.exists) {
+                fillTransactionScreen(doc.data());
+                toggleSaveButtonDisable();
+            } else {
+                alert("Documento nao encontrado");
+                window.location.href = "../home/home.html";
+            }
+        })
+        .catch(() => {
+            hideLoading();
+            alert("Erro ao recuperar documento");
+            window.location.href = "../home/home.html";
+        });
+}
+
+function fillTransactionScreen(transaction) {
+    if (transaction.type == "expense") {
+        form.typeExpense().checked = true;
+    } else {
+        form.typeIncome().checked = true;
+    }
+
+    form.date().value = transaction.date;
+    form.currency().value = transaction.money.currency;
+    form.value().value = transaction.money.value;
+    form.transactionType().value = transaction.transactionType;
+
+    if (transaction.description) {
+        form.description().value = transaction.description;
+    }
+}
+
+function saveTransaction() {
+    const transaction = createTransaction();
+
+    if (isNewTransaction()) {
+        save(transaction);
+    } else {
+        update(transaction);
+    }
+}
+
+function save(transaction) {
+    showLoading();
+
+    firebase.firestore()
+        .collection('transactions')
+        .add(transaction)
+        .then(() => {
+            hideLoading();
+            window.location.href = "../home/home.html";
+        })
+        .catch(() => {
+            hideLoading();
+            alert('Erro ao salvar transaçao');
+        })
+}
+
+function update(transaction) {
+    showLoading();
+    firebase.firestore()
+        .collection("transactions")
+        .doc(getTransactionUid())
+        .update(transaction)
+        .then(() => {
+            hideLoading();
+            window.location.href = "../home/home.html";
+        })
+        .catch(() => {
+            hideLoading();
+            alert('Erro ao atualizar transaçao');
+        });
+}
+
+function createTransaction() {
+    return {
+        type: form.typeExpense().checked ? "expense" : "income",
+        date: form.date().value,
+        money: {
+            currency: form.currency().value,
+            value: parseFloat(form.value().value)
+        },
+        transactionType: form.transactionType().value,
+        description: form.description().value,
+        user: {
+            uid: firebase.auth().currentUser.uid
+        }
+    };
 }
 
 function onChangeDate() {
     const date = form.date().value;
     form.dateRequiredError().style.display = !date ? "block" : "none";
 
-    toggleSaveButtonDisable()
-    
+    toggleSaveButtonDisable();
 }
 
-function onChangeValue(){
-const value = form.value().value;
-form.valueRequiredError().style.display = !value ? 'block' : 'none';
+function onChangeValue() {
+    const value = form.value().value;
+    form.valueRequiredError().style.display = !value ? "block" : "none";
 
-form.valueLessOrEqualToZeroError().style.display = value <= 0 ? 'block' : 'none';
+    form.valueLessOrEqualToZeroError().style.display = value <= 0 ? "block" : "none";
 
-toggleSaveButtonDisable()
+    toggleSaveButtonDisable();
 }
 
-function onChangeTransactionType(){
-const transactionType = form.transactionType().value;
+function onChangeTransactionType() {
+    const transactionType = form.transactionType().value;
+    form.transactionTypeRequiredError().style.display = !transactionType ? "block" : "none";
 
-form.transactionTypeRequiredError().style.display = !transactionType ? 'block' : 'none'
-
-toggleSaveButtonDisable()
+    toggleSaveButtonDisable();
 }
 
-function toggleSaveButtonDisable(){
+function toggleSaveButtonDisable() {
     form.saveButton().disabled = !isFormValid();
-
 }
 
 function isFormValid() {
@@ -60,41 +170,3 @@ function isFormValid() {
 
     return true;
 }
-
-
-function saveTransaction() {
-    showLoading();
-
-    const transaction = createTransaction();
-
-    firebase.firestore()
-        .collection('transactions')
-        .add(transaction)
-        .then(() => {
-            hideLoading();
-            window.location.href = "../home/home.html";
-        })
-        .catch(() => {
-            hideLoading();
-            alert('Erro ao salvar transaçao');
-        })
-}
-
-function createTransaction() {
-    return {
-        type: form.typeExpense().checked ? "expense" : "income",
-        date: form.date().value,
-        money: {
-            currency: form.currency().value,
-            value: parseFloat(form.value().value)
-        },
-        transactionType: form.transactionType().value,
-        description: form.description().value,
-        user: {
-            uid: firebase.auth().currentUser.uid
-        }
-    };
-}
-
-
-
